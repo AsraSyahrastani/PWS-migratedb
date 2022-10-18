58782c6df9f99746485373115071908cd96867fe
AsraSyahrastani <asra.s.ft20@mail.umy.ac.id>
2022-10-18 23:06:03 +0700

    first commit

---
+++ src/main/java/pabd/pindahdb/PindahdbApplication.java
@@ -0,0 +1,13 @@
+package pabd.pindahdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PindahdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PindahdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pabd/pindahdb/PindahdbApplicationTests.java
@@ -0,0 +1,13 @@
+package pabd.pindahdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PindahdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
